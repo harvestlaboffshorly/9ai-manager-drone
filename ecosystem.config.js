c12e03354e9a4c5d73e798df83edc474628ec45e
migs <miguel@offshorly.com>
2025-11-07 13:17:56 +0800

    user server.js instead of main

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "9ai-drone",
-      script: "dist/main.js",
+      script: "dist/server.js",
       cwd: "/apps/9ai-manager-drone",
       env: {
         NODE_ENV: "production",
